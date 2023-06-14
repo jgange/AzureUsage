a900a9b143cf60b8e5ca13d681dd35b4f3790741
jgange <jgange@users.noreply.github.com>
2023-06-14 14:18:41 -0500

    fixed display of users

--- getPrivilegedRoles.ps1
+++ getPrivilegedRoles.ps1
@@ -1,7 +1,4 @@
-﻿#Connect-AzAccount
-#Update-AzConfig -DisplayBreakingChangeWarning $false
-
-<#
+﻿<#
    Show Resource group ownership for every resource group in every subscription
    Generate a report that shows Subscription for the header
    Add a blank line
@@ -27,10 +24,18 @@ function DisplayReport($subscription)
    {
       $resourceGroupName = $resourceGroup.ResourceGroupName
       DisplayHeader "Resource Group: $resourceGroupName`n-------------------------------------`n"
-      $ra = Get-AzRoleAssignment -ResourceGroupName $resourceGroupName | Where-Object {($_.RoleDefinitionName -eq "Owner") -or ($_.RoleDefinitionName -eq "Contributor")} | Select-Object -Property DisplayName,SignInName,RoleDefinitionName,ObjectType,CanDelegate
-      DisplayDetail $ra
-      $ra.DisplayName | ForEach-Object { try { $identities.Add($_.ToString().Trim()) | Out-Null }
-         catch {}
+      $roleAssignment = Get-AzRoleAssignment -ResourceGroupName $resourceGroupName | Where-Object {($_.RoleDefinitionName -eq "Owner") -or ($_.RoleDefinitionName -eq "Contributor")} | Select-Object -Property DisplayName,SignInName,RoleDefinitionName,ObjectType,CanDelegate
+      DisplayDetail $roleAssignment
+
+      $roleAssignment | ForEach-Object {
+         $roleRecord = [PSCustomObject]@{
+            DisplayName        = $_.DisplayName
+            SignInName         = $_.SignInName
+            RoleDefinitionName = $_.RoleDefinitionName
+            ObjectType         = $_.ObjectType
+            CanDelete          = $_.CanDelegate
+        }
+        $identities.Add($roleRecord) | Out-Null
       }
    }
    Write-Output "`n"
@@ -46,12 +51,45 @@ function DisplayDetail($detailTable)
    $detailTable | Format-Table -AutoSize
    Write-Output "`n"
 }
-#### Main Program ####
-Start-Transcript -Path "./AzureRoles.txt"
 
-Get-AzSubscription | ForEach-Object { DisplayReport $_ }
+function displayUserList($identities)
+{
+   $identities | Sort-Object -Property DisplayName -unique | Format-Table -AutoSize
+}
+
+function ShowExpiredUsers()
+{
+      # Get-AzureADUser -ObjectId "testUpn@tenant.com"
+}
+
+function ShowActiveUsers()
+{
+
+}
+
+function getGroupMembers()
+{
+
+}
+
+#### Main Program ####
+[void](Update-AzConfig -DisplayBreakingChangeWarning $false)
+
+<#
+try {
+   Get-AzureADTenantDetail
+}
+catch {
+   Connect-AzAccount
+}
+#>
+
+# Start-Transcript -Path "./AzureRoles.txt"
+
+Get-AzSubscription | Select-Object -First 1 | ForEach-Object { DisplayReport $_ }
 
 Write-Output "List of identities included in the report:`n"
-$identities | Sort-Object -Unique
 
-Stop-Transcript
+displayUserList $identities
+
+# Stop-Transcript
